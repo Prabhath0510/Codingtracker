c7a135fde7f9eeb685cf839a6d03ba961072a6c7
Prabhath0510 <159057401+Prabhath0510@users.noreply.github.com>
2024-02-05 16:53:29 -0500

    Update Program.cs
    KEEPING ONE MESSAGE

--- Codingtracker/Program.cs
+++ Codingtracker/Program.cs
@@ -1,6 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, c#! one more modification"); Console.WriteLine("Hello, c#! one more modification");
-Console.WriteLine("Hello, c#! one more modification"); Console.WriteLine("Hello, c#! one more modification");
-Console.WriteLine("Hello, c#! one more modification"); VConsole.WriteLine("Hello, c#! one more modification"); Console.WriteLine("Hello, c#! one more modification");
-kl
-    HHLKH
+Console.WriteLine("Hello, c#! one more modification");
