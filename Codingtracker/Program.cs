5a9c2a3f84907f7a4034351093be235aa8d95daf
raaamesh moops <rmopuri@gmail.com>
2024-02-05 16:50:59 -0500

    NOW WE HAVE MANY MESSAGES

--- Codingtracker/Program.cs
+++ Codingtracker/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, c#! one more modification");
+Console.WriteLine("Hello, c#! one more modification"); Console.WriteLine("Hello, c#! one more modification");
+Console.WriteLine("Hello, c#! one more modification"); Console.WriteLine("Hello, c#! one more modification");
+Console.WriteLine("Hello, c#! one more modification"); VConsole.WriteLine("Hello, c#! one more modification"); Console.WriteLine("Hello, c#! one more modification");
 kl
